ebc0add76d4b8db15020577f7742ddd755e12267
luisAgt <lagustinf2000@alumno.ipn.mx>
2026-05-20 09:14:42 -0600

    Init project.

---
+++ src/main/java/com/equipo1/assistance_sys/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package com.equipo1.assistance_sys.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
